ec842c0b39a45b00d27d9cce775d575ec13105bc
Rayan Gustavo <rayang@cpqd.com.br>
2025-11-15 12:23:52 -0300

    alterado para usar EnumerableSet

--- src/ValidatorSelection.sol
+++ src/ValidatorSelection.sol
@@ -1,11 +1,15 @@
 pragma solidity ^0.8.13;
 
 import "src/IValidatorSelection.sol";
+import "src/permissioning/Governable.sol";
+import "src/permissioning/AdminProxy.sol";
+import "src/permissioning/NodeRulesV2Impl.sol";
 
 contract ValidatorSelection is IValidatorSelection {
-    address[] public elegibleValidators;
-    address[] public operationalValidators;
-    mapping (address => bool) public isOperational;
+    using EnumerableSet for EnumerableSet.AddressSet;
+
+    EnumerableSet.AddressSet private elegibleValidators;
+    EnumerableSet.AddressSet private operationalValidators;
 
     uint16 public blocksBetweenSelection;
     uint16 public blocksWithoutProposeThreshold;
@@ -18,9 +22,8 @@ contract ValidatorSelection is IValidatorSelection {
 
     function monitorsValidators() external {
         address proposer = block.coinbase;
-        require(lastBlockProposedBy[proposer] != block.number, "Monitoramento ja executado neste bloco.");
+        require(lastBlockProposedBy[proposer] != block.number, "Monitoring already executed in this block.");
         lastBlockProposedBy[proposer] = block.number;
-        isOperational[proposer] = true;
         emit MonitorExecuted(msg.sender);
 
         if (block.number % blocksBetweenSelection == 0){
@@ -30,10 +33,10 @@ contract ValidatorSelection is IValidatorSelection {
 
     function _selectValidators() internal {
         uint index = 0;
-        while (index < operationalValidators.length) {
-            address candidateValidator = operationalValidators[index];
+        while (index < operationalValidators.length()) {
+            address candidateValidator = operationalValidators.at(index);
             if (block.number - lastBlockProposedBy[candidateValidator] > blocksWithoutProposeThreshold) {
-                _removeValidatorByIndex(index);
+                _removeOperationalValidatorByIndex(index);
             } else {
                 index++;
             }
@@ -41,33 +44,42 @@ contract ValidatorSelection is IValidatorSelection {
         emit SelectionExecuted(msg.sender);
     }
 
-    function _removeValidatorByIndex(uint256 _index) internal {
-        address validatorToRemove = operationalValidators[_index];
-        isOperational[validatorToRemove] = false;
-
-        uint lastIndex = operationalValidators.length - 1;
-        if (_index != lastIndex) {
-            operationalValidators[_index] = operationalValidators[lastIndex];
-        }
-        operationalValidators.pop();
+    function _removeOperationalValidatorByIndex(uint256 _index) internal {
+        address validatorToRemove = operationalValidators.at(_index);
+        operationalValidators.remove(validatorToRemove);
         emit ValidatorRemoved(validatorToRemove);
     }
 
     function setBlocksBetweenSelection(uint16 _blocksBetweenSelection) external {
-        require(_blocksBetweenSelection > 0, "Blocos entre selecao deve ser > 0.");
+        require(_blocksBetweenSelection > 0, "Blocks between selection must be > 0.");
         blocksBetweenSelection = _blocksBetweenSelection;
     }
 
     function setBlocksWithoutProposeThreshold(uint16 _blocksWithoutProposeThreshold) external {
-        require(_blocksWithoutProposeThreshold > 0, "Limite de blocos sem proposta do validador deve ser > 0.");
+        require(_blocksWithoutProposeThreshold > 0, "The limit for blocks without a validator proposal must be > 0.");
         blocksWithoutProposeThreshold = _blocksWithoutProposeThreshold;
     }
 
     function getActiveValidators() external view returns (address[] memory) {
-        return operationalValidators;
+        return operationalValidators.values();
     }
 
     function addOperationalValidator(address _validator) public {
-        operationalValidators.push(_validator);
+        require(elegibleValidators.contains(_validator) == true, "This node is not eligible");
+        operationalValidators.add(_validator);
+    }
+
+    function addElegibleValidator(address _validator) public {
+        elegibleValidators.add(_validator);
+    }
+
+    function removeOperationalValidator(address _validator) public {
+        require(operationalValidators.contains(_validator) == true, "This node is not operational");
+        operationalValidators.remove(_validator);
+    }
+
+    function removeElegibleValidator(address _validator) public {
+        require(elegibleValidators.contains(_validator) == true, "This node is not eligible");
+        elegibleValidators.remove(_validator);
     }
 }
