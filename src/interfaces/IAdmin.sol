ab74dc2257ae94a987547ece6833a681a312f75b
Rayan Gustavo <rayang@cpqd.com.br>
2025-11-25 09:16:35 -0300

    alterando versão do compilador

--- src/interfaces/IAdmin.sol
+++ src/interfaces/IAdmin.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-only
-pragma solidity 0.8.28;
+pragma solidity ^0.8.13;
 
 interface IAdmin {
     function isAuthorized(address _address) external view returns (bool);
