6faf4a56153ff4e8e8f1b354912b9b40fca2166c
Rayan Gustavo <rayang@cpqd.com.br>
2025-11-27 09:18:56 -0300

    adicionando interface para o contrato de seleção

--- src/interfaces/IValidatorSelection.sol
+++ src/interfaces/IValidatorSelection.sol
@@ -1,5 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.13;
 
 interface IValidatorSelection {
+    function monitorsValidators() external;
+    function setBlocksBetweenSelection(uint16 _blocksBetweenSelection) external;
+    function setBlocksWithoutProposeThreshold(uint16 _blocksWithoutProposeThreshold) external;
     function getActiveValidators() external view returns (address[] memory);
+    function addElegibleValidator(address _validator) external;
+    function removeElegibleValidator(address _validator) external;
+    function addOperationalValidator(bytes32 enodeHigh, bytes32 enodeLow) external;
+    function removeOperationalValidator(bytes32 enodeHigh, bytes32 enodeLow) external;
 }
