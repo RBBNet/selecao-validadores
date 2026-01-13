3478725f0b8982823b0268612a5d63ac62940743
Rayan Gustavo <rayang@cpqd.com.br>
2026-01-13 08:57:06 -0300

    alterando versão do compilador para a versão 0.8.33

--- src/ValidatorSelection.sol
+++ src/ValidatorSelection.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-only
-pragma solidity ^0.8.13;
+pragma solidity 0.8.33;
 
 import {IValidatorSelection} from "src/interfaces/IValidatorSelection.sol";
 import {IAdminProxy} from "src/interfaces/IAdminProxy.sol";
